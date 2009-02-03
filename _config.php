959ed3dae4ee5abb8639ef919fa6f1d877845845
Saophalkun Ponlu <phalkunz@silverstripe.com>
2009-02-03 21:42:23 +0000

    removed mollom public and private keys

--- _config.php
+++ _config.php
@@ -1,8 +1,9 @@
 <?php
 require_once("lib/mollom.php");
 
-Mollom::setPublicKey("1819023dfcb10a667d10bd1578c5f39b");
-Mollom::setPrivateKey("2430fcbd65756ac6732a54a7c9ada116");
+Mollom::setPublicKey("enter-your-mollom-public-key");
+Mollom::setPrivateKey("enter-your-mollom-private-key");
+?>
 
 
 
