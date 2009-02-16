77b65646d317f7e5b7ba09977cb9b579a87ae675
Will Rossiter <will@silverstripe.com>
2009-02-16 00:29:16 +0000

    FEATURE: added basic mark as spam filter

--- code/MollomSpamProtector.php
+++ code/MollomSpamProtector.php
@@ -13,13 +13,26 @@ class MollomSpamProtector {
 		if ($before && $form->Fields()->fieldByName($before)) {
 			$form->Fields()->insertBefore($this->mollomField, $before);
 		}
-		else {
+		else { 
 			$form->Fields()->push($this->mollomField);
 		}
 	}
 	
 	function setFieldMapping($fieldToPostTitle, $fieldsToPostBody, $fieldToAuthorName=null, $fieldToAuthorUrl=null, $fieldToAuthorEmail=null, $fieldToAuthorOpenId=null) {
 		$this->mollomField->setFieldMapping($fieldToPostTitle, $fieldsToPostBody, $fieldToAuthorName, $fieldToAuthorUrl, $fieldToAuthorEmail, $fieldToAuthorOpenId);
+	}
+	
+	/**
+	 * Mark Item as Spam. 
+	 * 
+	 * @todo {@link Mollom::sendFeedback()} second parameter accepts
+	 * different request types. spam, profanity, low-quality, unwanted yet we 
+	 * only tag them as SPAM.
+	 */
+	function markAsSpam() {
+		$sessionID = Session::get("mollom_session_id") ? Session::get("mollom_session_id") : null;
+		MollomServer::initServerList();
+		return Mollom::sendFeedback($sessionID, 'spam');
 	}
 }
 
