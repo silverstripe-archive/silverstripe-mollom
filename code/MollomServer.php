06905235d6ac6485a69322065db9f23bd605f479
Saophalkun Ponlu <phalkunz@silverstripe.com>
2009-03-11 04:53:32 +0000

    ENHANCEMENT: handling outdated server list

--- code/MollomServer.php
+++ code/MollomServer.php
@@ -1,7 +1,7 @@
 <?php
 /** 
  * This class is responsible for caching a list of Mollom servers
- * @TODO: try and catch every remote method call
+ * @NOTE: Call to Mollom class's method should go through Mollom::doCall
  */
 class MollomServer extends DataObject {
 	static $db = array(
@@ -22,10 +22,22 @@ class MollomServer extends DataObject {
 		return false;
 	}
 	
+	protected function clearCachedServerList() {
+		$list = DataObject::get("MollomServer");
+		if ($list) {
+			foreach ($list as $server) {
+				$server->delete();
+			}
+		}
+	}
+	
 	static function getServerList() {
 		if (self::getCachedServerList()) return self::getCachedServerList();
 		
-		$servers = Mollom::getServerList();
+		$servers = self::doCall("getServerList", null);
+		
+		if (!$servers) return null;
+		
 		foreach ($servers as $server) {
 			$mollomServer = new MollomServer();
 			$mollomServer->ServerURL = $server;
@@ -37,30 +49,60 @@ class MollomServer extends DataObject {
 	}
 	
 	static function verifyKey() {
-		return Mollom::verifyKey();
+		return self::doCall("verifyKey", null);
 	}
 	
 	/**
 	 * @param 	array of server urls
 	 */
 	static function initServerList() {
-		Mollom::setServerList(self::getServerList()); 
+		self::doCall("setServerList", array(self::getServerList())); 
 	}
 	
 	static function getImageCaptcha($sessionId=null) {
-		return Mollom::getImageCaptcha($sessionId);
+		return self::doCall("getImageCaptcha", array($sessionId));
 	}
 	
 	static function getAudioCaptcha($sessionId=null) {
-		return Mollom::getAudioCaptcha($sessionId);
+		return self::doCall("getAudioCaptcha", array($sessionId));
 	}
 	
 	static function checkCaptcha($sessionId, $solution) {
-		return Mollom::checkCaptcha($sessionId, $solution);
+		return self::doCall("checkCaptcha", array($sessionId, $solution));
 	}
 	
 	static function checkContent($session_id=null, $postTitle=null, $postBody=null, $authorName=null, $authorUrl=null, $authorEmail=null, $authorOpenId=null) {
-		return Mollom::checkContent($session_id, $postTitle, $postBody, $authorName, $authorUrl, $authorEmail, $authorOpenId);
+		return self::doCall("checkContent", array($session_id, $postTitle, $postBody, $authorName, $authorUrl, $authorEmail, $authorOpenId));
 	}
+	
+	/**
+	 * All mollom service method calls should go through this method 
+	 * in order to try-and-catch and handle exceptions on one place 
+	 * @param 	string 		name of the function 
+	 * @param 	array 		array of the function's parameter 
+	 * @return 	mixed 
+	 */
+	protected static function doCall($name, $params=null) {
+		try {
+			return call_user_func_array( 'Mollom::' . $name, $params);
+		}
+		catch (Exception $e) {
+			$errCode = $e->getCode();
+			switch ($errCode) {
+				// if the cached serverlist is outdated
+				case 1100:
+					// delete cached server list first - in database
+					singleton('MollomServer')->clearCachedServerList();
+					// use default server list 
+					Mollom::setServerList(Mollom::getServerList());
+					break;
+					
+				default:
+					throw new Exception( $e->getMessage() , $errCode);
+			}
+			
+		}
+	}
+	
 }
 ?>
