6fe6828b93a3bdb47bb7560136b6cb7a1bd5a73d
Will Rossiter <will@silverstripe.com>
2009-03-12 21:56:26 +0000

    MINOR: added link to the list of molloms conditions and policys page

--- code/MollomReport.php
+++ code/MollomReport.php
@@ -15,6 +15,7 @@ class MollomReport extends SSReport {
 		// check a key has been configured
 		MollomServer::initServerList();
 		
+		// check to see what error we should report
 		if(Mollom::getPublicKey()) {
 			// check it is valid
 			if(Mollom::verifyKey()) {
@@ -27,9 +28,13 @@ class MollomReport extends SSReport {
 		else {
 			$msg = _t('Mollom.NOKEY', 'You have not configured your key. Please add it to mysite/_config.php.');
 		}
+		
+		// Link to the list of conditions
+		$terms = "<a href='http://mollom.com/terms-of-service' target='_blank'>". _t('Mollom.TERMS', 'View Molloms Terms of Use'). "</a>";
+		
 		$fields = new FieldSet(
 			new LiteralField('MollomKeyWorks',
-				'<h3>'. _t('Mollom.KEYSTATUS','Mollom Key Status') .'</h3><p>'. $msg .'</p>'
+				'<h3>'. _t('Mollom.KEYSTATUS','Mollom Key Status') .'</h3><p>'. $msg .'</p><p>'. $terms .'</p>'
 			),
 			new LiteralField('MollomStatus', "
 				<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0\" width=\"100%\" height=\"100%\" id=\"movie\">
