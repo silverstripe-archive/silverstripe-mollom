d92c502bbf204bca6ea04b56dfd47a228d38efbe
Saophalkun Ponlu <phalkunz@silverstripe.com>
2010-04-22 01:34:07 +0000

    BUGFIX: mollom report

--- _config.php
+++ _config.php
@@ -1,1 +1,2 @@
 <?php
+SS_Report::register('ReportAdmin', 'MollomReport');
--- code/MollomReport.php
+++ code/MollomReport.php
@@ -6,45 +6,49 @@
  * @subpackage mollom
  */
 
-class MollomReport extends SSReport {
+class MollomReport extends SS_Report {
 	
 	protected $title = "Mollom";
 	
 	protected $description = "Mollom Usage and Statistics";
 	
+	/**
+	 * Returns empty field set and use echo() to return output instead. 
+	 *
+	 * Using form fields and fieldset can done but we need to introduce FieldGroup (according to the ReportAdmin form template, {@see ReportAdminForm.ss}).
+	 * On top of that, we need to css to style the output profile in order to produce a readable output. 
+	 */
 	function getCMSFields() {
 		$URL = "http://mollom.com/statistics.swf?key=". Mollom::getPublicKey();
-		$msg = "";
-		// check a key has been configured
+		$errMsg = "";
+		
 		MollomServer::initServerList();
 		
-		// check to see what error we should report
 		if(Mollom::getPublicKey()) {
-			// check it is valid
-			if(Mollom::verifyKey()) {
-				$msg = _t('Mollom.KEYEXISTS', 'Your key exists and is working');
-			}
-			else {
-				$msg = _t('Mollom.KEYWRONG', 'Your key is not valid. Please try another key');
+			if(!MollomServer::verifyKey()) {
+				$errMsg = _t('Mollom.KEYWRONG', 'Your key is not valid. Please try another key');
 			}
 		}
 		else {
-			$msg = _t('Mollom.NOKEY', 'You have not configured your key. Please add it to mysite/_config.php.');
+			$errMsg = _t('Mollom.NOKEY', 'You have not configured your key. Please add it to mysite/_config.php.');
 		}
+
+		echo "<h3>Mollom Key Status</h3>";
 		
-		// Link to the list of conditions
-		$terms = "<a href='http://mollom.com/terms-of-service' target='_blank'>". _t('Mollom.TERMS', 'View Molloms Terms of Use'). "</a>";
-		
-		$fields = new FieldSet(
-			new LiteralField('MollomKeyWorks',
-				'<h3>'. _t('Mollom.KEYSTATUS','Mollom Key Status') .'</h3><p>'. $msg .'</p><p>'. $terms .'</p>'
-			),
-			new LiteralField('MollomStatus', "
+		if (empty($errMsg)) {
+			echo "
+				<p><a href='http://mollom.com/terms-of-service' target='_blank'>". _t('Mollom.TERMS', 'View Molloms Terms of Use'). "</a></p>
+			
 				<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0\" width=\"100%\" height=\"100%\" id=\"movie\">
 					<param name=\"movie\" value=\"$URL\">
 					<embed src=\"$URL\" quality=\"high\" width=\"100%\" height=\"100%\" name=\"movie\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\"> 
-				</object>")
-		);
-		return $fields;
+				</object>
+			";
+		}
+		else {
+			echo "<p>$errMsg</p>";
+		}
+		
+		return new FieldSet();
 	}
 }
